c779f1bf5fd72f1a4449a761aff9ed0d31ea91c1
huyihsuan <huyihsuan@gmail.com>
2021-04-13 22:27:29 +0800

    fix style

--- src/reverseNest.ts
+++ src/reverseNest.ts
@@ -1,9 +1,6 @@
 
 const reverseNest = (nest: object, preObj: object | string = null) => {
-    if (!nest) throw new TypeError("invalid input");
-    const nestArr: [string, any][] = Object.entries(nest);
-    if (nestArr.length !== 1) throw new TypeError("invalid input");
-    const [key, value] = nestArr[0];
+    const [key, value] = entriesObject(nest);
     const newValue = preObj ? { [key]: preObj } : key;
     if (value && typeof value === 'object') {
         return reverseNest(value, newValue)
@@ -11,6 +8,13 @@ const reverseNest = (nest: object, preObj: object | string = null) => {
         if (typeof value !== "string") throw new TypeError("invalid input");
         return { [value]: newValue }
     };
+}
+
+const entriesObject = (nest: object) => {
+    if (!nest) throw new TypeError("invalid input");
+    const nestArr: [string, any][] = Object.entries(nest);
+    if (nestArr.length !== 1) throw new TypeError("invalid input");
+    return nestArr[0];
 }
 
 module.exports = reverseNest;
